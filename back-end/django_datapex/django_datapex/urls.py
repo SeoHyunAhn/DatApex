2da44a40d00449908698dbfd1e307f38a7369f96
Harsha Valluri <harsha.valluri@Harshas-MacBook-Pro.local>
2019-10-03 19:00:37 -0400

    minor change

--- back-end/django_datapex/django_datapex/urls.py
+++ back-end/django_datapex/django_datapex/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls'))
 ]
